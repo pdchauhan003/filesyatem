a2c80f8c814e45576ff2b441304703b228052ff7
pdchauhan003 <pdchauhan003@gmail.com>
2026-04-15 14:59:59 +0530

    update production db

--- Backend/db/postgress.js
+++ Backend/db/postgress.js
@@ -1,10 +1,22 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
+
+// export const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: process.env.DATABASE,
+//     password: process.env.PASSWORD,
+//     port: 5432
+
+// })
+
+
+
+import pkg from 'pg';
+const { Pool } = pkg;
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: 5432
-
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
