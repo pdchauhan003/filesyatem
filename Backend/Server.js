9030239cddd6f8be3d6f264c446c8ed4a94fba57
pdchauhan003 <pdchauhan003@gmail.com>
2026-04-15 12:55:22 +0530

    update backend

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,9 +10,6 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads')),
     app.use('/api', uploadRoutes)
 
-app.use('/', (req, res) => {
-    res.send('hello')
-})
 
 app.listen(1312, () => {
     console.log('server run in 1312 port');
